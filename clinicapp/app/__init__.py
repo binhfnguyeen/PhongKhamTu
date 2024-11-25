06cf687c0122db4cbb45c8a90aef2ba16cb92401
nguyendzaii16112004@gmail.com <nguyendzaii16112004@gmail.com>
2024-11-25 21:10:24 +0700

    add class models

--- clinicapp/app/__init__.py
+++ clinicapp/app/__init__.py
@@ -3,6 +3,6 @@ from urllib.parse import quote
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = "mysql+pymysql://root:%s@localhost/benhvientu?charset=utf8mb4" % quote('Nguyennguyen123@')
+app.config["SQLALCHEMY_DATABASE_URI"] = "mysql+pymysql://heulwen:%s@localhost/benhvientu?charset=utf8mb4" % quote('Nguyennguyen123@')
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = True
 db = SQLAlchemy(app)
