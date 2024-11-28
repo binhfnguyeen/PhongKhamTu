33d10ffbe75d45815f183139a4bd1e734f4cba86
“MinhTri07” <minhtri472k4@gmail.com>
2024-11-28 19:00:06 +0700

    tao template + base

--- clinicapp/app/__init__.py
+++ clinicapp/app/__init__.py
@@ -6,3 +6,4 @@ app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "mysql+pymysql://root:%s@localhost/benhvientu?charset=utf8mb4" % quote('Nguyennguyen123@')
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = True
 db = SQLAlchemy(app)
+
---
+++ clinicapp/app/index.py
@@ -0,0 +1,31 @@
+from flask import Flask, render_template, request, redirect
+from clinicapp.app import app
+
+@app.route('/')
+def index():
+    return render_template('home.html')
+
+@app.route('/login')
+def login():
+    return render_template ('login.html')
+
+@app.route('/datlichkham')
+def datlichkham():
+    return render_template('datlichkham.html')
+
+@app.route('/lapphieukham')
+def lapphieukham():
+    return render_template('lapphieukham.html')
+@app.route('/baocaothongke')
+def baocaothongke():
+    return render_template('baocaothongke.html')
+
+@app.route('/thanhtoan')
+def thanhtoan():
+     return render_template('thanhtoan.html')
+
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
